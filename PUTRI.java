400570671da4bfc6b59bdc62331e253e4188bb51
putri2009 <puspadewi1508@gmail.com>
2025-07-24 08:06:57 +0700

    Add files via upload

---
+++ PUTRI.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package putri;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PC 11-LAB R1
+ */
+public class PUTRI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        // TODO code application logic here
+//        System.out.println("HALLO DUNIA");
+//        System.out.println("HALLO"+"DUNIA");
+//        System.out.println(2+" "+5);
+//        System.out.println(2+5);
+//        System.out.println("Angka "+" "+5);
+//        System.out.println("2 + 5"+" = +2+5");
+//        System.out.println("2 + 5"+" = "+ (2+5));
+//String namaDepan = "";
+//String namaBelakang = "";
+//Scanner input = new Scanner(System.in);
+//System.out.println("Nama Depan : ");
+////membaca inputan user
+//namaDepan = input.next();
+//System.out.println("Nama Belakang : ");
+////membaca inputan user
+//namaBelakang = input.next();
+//System.out.println("Nama Saya adalah :");
+//System.out.println(namaDepan + " " + namaBelakang);  
+//String nama =JOptionPane.showInputDialog("Masukan Nama:");
+//String a =JOptionPane.showInputDialog("Masukan nilai untuk variabel a : ");
+//
+//String b =JOptionPane.showInputDialog("Masukan nilai untuk variabel b : ");
+//
+//System.out.println("Hasil :" +
+//(Integer.parseInt(a) *
+//Integer.parseInt(b)));
+//String nama = "indah";
+//String jurusan = "Rekayasa Perangkat Lunak";
+//JOptionPane.showMessageDialog(null, "Halo " + nama
+//+ ",jurusan Kamu " + jurusan);
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Informasi.","Informasi",
+//JOptionPane.INFORMATION_MESSAGE);
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Error !","Error",
+//JOptionPane.ERROR_MESSAGE);
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Peringatan.","Peringatan",
+//JOptionPane.WARNING_MESSAGE);
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Pertanyaan.", "Pertanyaan",
+//JOptionPane.QUESTION_MESSAGE);
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Plain Message.","Plain Message",
+//JOptionPane.PLAIN_MESSAGE);
+//JOptionPane.showConfirmDialog(null, "Pilih Yes atau No?", "Pertanyaan",JOptionPane.YES_NO_OPTION);
+{ int pertanyaan =
+JOptionPane.showConfirmDialog(null, "Pilih Yes atau No?", "Pertanyaan",JOptionPane.YES_NO_OPTION);
+
+if(JOptionPane.YES_OPTION == pertanyaan){
+JOptionPane.showMessageDialog(null, "Kamu memilih Yes.");
+}else if(JOptionPane.NO_OPTION == pertanyaan){
+JOptionPane.showMessageDialog(null, "Kamu memilih No.");
+}}
+    }  
+}
+
+
+
